82ef4de706c07e16caf3c26d3bc566130f7816a9
Dennis Irorere <denironyx@gmail.com>
2020-08-16 08:34:56 +0100

    error status and  messages

--- R/restcountries_api.R
+++ R/restcountries_api.R
@@ -5,19 +5,28 @@ restcountries_api <- function(path){
   url <- paste0("https://restcountries.eu/rest/v2/", path = path)
 
   resp = GET(url, ua)
-  if (http_type(resp) != "application/json") {
-    stop("API did not return json", call. = FALSE)
-  }
-  parsed <- jsonlite::fromJSON(content(resp, "text"), simplifyVector = TRUE)
 
-  if(status_code(resp) != 200){
+  if(http_error(resp)){
+
+    error_status <- httr::http_status(resp)
+
     stop(
       sprintf(
-        "RESTCountries API request failed"
+        "RESTCountries API request failed %s\nmessage: %s\ncategory: %s \nurl: %s",
+        error_status$message,
+        error_status$category,
+        error_status$reason,
+        url
       ),
       call. = FALSE
     )
   }
+
+  if (http_type(resp) != "application/json") {
+    stop("API call executed successfully, but did not return expected json format",
+         call. = FALSE)
+  }
+  parsed <- jsonlite::fromJSON(content(resp, "text"), simplifyVector = TRUE)
 
   structure(
     list(
@@ -29,7 +38,7 @@ restcountries_api <- function(path){
   )
 }
 
-region_data <- restcountries_api("region/africa")
+region_data <- restcountries_api("region/afric")
 View(region_data$content)
 
 
