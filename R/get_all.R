667fa9e827f9a73744226394422da4bddbff1cf5
Dennis Irorere <denironyx@gmail.com>
2020-08-29 16:41:27 +0100

    restcountries api calls wrapper

--- R/get_all.R
+++ R/get_all.R
@@ -102,7 +102,311 @@ get_countries_by_lang <- function(x){
   out_df
 }
 
+get_countries_by_name <- function(x){
 
+  df <- restcountries_api(paste0("name/", x, "?fullText=true"))
+
+  ## remove the errored returns
+  df_index <- sapply(df, is.data.frame)
+  out_list  <- df[!df_index]
+
+  ## defaultName" - "newName"
+  out_cols <- c(
+    "country_name" = "name",
+    "domain" = "topLevelDomain",
+    "iso2c" = "alpha2Code",
+    "iso3c" = "alpha3Code",
+    "calling_codes" = "callingCodes",
+    "alt_spelling" = "altSpellings",
+    "sub_region" = "subregion",
+    "native_name" = "nativeName",
+    "regional_blocs" = "regionalBlocs",
+    "numeric_code" = "numericCode"
+  )
+
+  if(length(out_list) == 0){
+
+    warning("No data was returned for any requested country. Returning empty data frame")
+    out_df <- as.data.frame(matrix(nrow = 0, ncol = length(out_cols)))
+    names(out_df) <- names(out_cols)
+
+  } else {
+
+    out_df <- out_list %>%
+      rename(
+        all_of(out_cols)
+      ) %>%
+      hoist(latlng,
+            lat = list(1),
+            lon = list(2)
+      ) %>%
+      unnest(languages, names_sep = "_")
+  }
+  out_df
+
+}
+
+
+get_countries_by_currency <- function(input){
+
+  df <- restcountries_api(paste0("currency/", input))
+
+  ## remove the errored returns
+  df_index <- sapply(df, is.data.frame)
+  out_list  <- df[!df_index]
+
+  ## defaultName" - "newName"
+  out_cols <- c(
+    "country_name" = "name",
+    "domain" = "topLevelDomain",
+    "iso2c" = "alpha2Code",
+    "iso3c" = "alpha3Code",
+    "calling_codes" = "callingCodes",
+    "alt_spelling" = "altSpellings",
+    "sub_region" = "subregion",
+    "native_name" = "nativeName",
+    "regional_blocs" = "regionalBlocs",
+    "numeric_code" = "numericCode"
+  )
+
+  if(length(out_list) == 0){
+
+    warning("No data was returned for any requested country. Returning empty data frame")
+    out_df <- as.data.frame(matrix(nrow = 0, ncol = length(out_cols)))
+    names(out_df) <- names(out_cols)
+
+  } else {
+
+    out_df <- out_list %>%
+      rename(
+        all_of(out_cols)
+      ) %>%
+      hoist(latlng,
+            lat = list(1),
+            lon = list(2)
+      ) %>%
+      unnest(currencies, names_sep = "_")
+  }
+  out_df
+
+}
+
+
+get_countries_by_capital <- function(input){
+
+  df <- restcountries_api(paste0("capital/", input))
+
+  ## remove the errored returns
+  df_index <- sapply(df, is.data.frame)
+  out_list  <- df[!df_index]
+
+  ## defaultName" - "newName"
+  out_cols <- c(
+    "country_name" = "name",
+    "domain" = "topLevelDomain",
+    "iso2c" = "alpha2Code",
+    "iso3c" = "alpha3Code",
+    "calling_codes" = "callingCodes",
+    "alt_spelling" = "altSpellings",
+    "sub_region" = "subregion",
+    "native_name" = "nativeName",
+    "regional_blocs" = "regionalBlocs",
+    "numeric_code" = "numericCode"
+  )
+
+  if(length(out_list) == 0){
+
+    warning("No data was returned for any requested country. Returning empty data frame")
+    out_df <- as.data.frame(matrix(nrow = 0, ncol = length(out_cols)))
+    names(out_df) <- names(out_cols)
+
+  } else {
+
+    out_df <- out_list %>%
+      rename(
+        all_of(out_cols)
+      ) %>%
+      hoist(latlng,
+            lat = list(1),
+            lon = list(2)
+      )
+  }
+  out_df
+
+}
+
+
+get_countries_by_region <- function(input){
+
+  df <- restcountries_api(paste0("region/", input))
+
+  ## remove the errored returns
+  df_index <- sapply(df, is.data.frame)
+  out_list  <- df[!df_index]
+
+  ## defaultName" - "newName"
+  out_cols <- c(
+    "country_name" = "name",
+    "domain" = "topLevelDomain",
+    "iso2c" = "alpha2Code",
+    "iso3c" = "alpha3Code",
+    "calling_codes" = "callingCodes",
+    "alt_spelling" = "altSpellings",
+    "sub_region" = "subregion",
+    "native_name" = "nativeName",
+    "regional_blocs" = "regionalBlocs",
+    "numeric_code" = "numericCode"
+  )
+
+  if(length(out_list) == 0){
+
+    warning("No data was returned for any requested country. Returning empty data frame")
+    out_df <- as.data.frame(matrix(nrow = 0, ncol = length(out_cols)))
+    names(out_df) <- names(out_cols)
+
+  } else {
+
+    out_df <- out_list %>%
+      rename(
+        all_of(out_cols)
+      ) %>%
+      hoist(latlng,
+            lat = list(1),
+            lon = list(2)
+      )
+  }
+  out_df
+
+}
+
+get_countries_by_callingcode <- function(input){
+
+  df <- restcountries_api(paste0("callingcode/", input))
+
+  ## remove the errored returns
+  df_index <- sapply(df, is.data.frame)
+  out_list  <- df[!df_index]
+
+  ## defaultName" - "newName"
+  out_cols <- c(
+    "country_name" = "name",
+    "domain" = "topLevelDomain",
+    "iso2c" = "alpha2Code",
+    "iso3c" = "alpha3Code",
+    "calling_codes" = "callingCodes",
+    "alt_spelling" = "altSpellings",
+    "sub_region" = "subregion",
+    "native_name" = "nativeName",
+    "regional_blocs" = "regionalBlocs",
+    "numeric_code" = "numericCode"
+  )
+
+  if(length(out_list) == 0){
+
+    warning("No data was returned for any requested country. Returning empty data frame")
+    out_df <- as.data.frame(matrix(nrow = 0, ncol = length(out_cols)))
+    names(out_df) <- names(out_cols)
+
+  } else {
+
+    out_df <- out_list %>%
+      rename(
+        all_of(out_cols)
+      ) %>%
+      hoist(latlng,
+            lat = list(1),
+            lon = list(2)
+      )
+  }
+  out_df
+}
+
+get_countries_by_blocs <- function(input){
+
+  df <- restcountries_api(paste0("regionalbloc/", input))
+
+  ## remove the errored returns
+  df_index <- sapply(df, is.data.frame)
+  out_list  <- df[!df_index]
+
+  ## defaultName" - "newName"
+  out_cols <- c(
+    "country_name" = "name",
+    "domain" = "topLevelDomain",
+    "iso2c" = "alpha2Code",
+    "iso3c" = "alpha3Code",
+    "calling_codes" = "callingCodes",
+    "alt_spelling" = "altSpellings",
+    "sub_region" = "subregion",
+    "native_name" = "nativeName",
+    "regional_blocs" = "regionalBlocs",
+    "numeric_code" = "numericCode"
+  )
+
+  if(length(out_list) == 0){
+
+    warning("No data was returned for any requested country. Returning empty data frame")
+    out_df <- as.data.frame(matrix(nrow = 0, ncol = length(out_cols)))
+    names(out_df) <- names(out_cols)
+
+  } else {
+
+    out_df <- out_list %>%
+      rename(
+        all_of(out_cols)
+      ) %>%
+      hoist(latlng,
+            lat = list(1),
+            lon = list(2)
+      )
+  }
+  out_df
+
+}
+
+get_countries_by_code <- function(input){
+
+  df <- restcountries_api(paste0("alpha/", "co"))
+
+  ## remove the errored returns
+  df_index <- sapply(df, is.data.frame)
+  out_list  <- df[!df_index]
+
+  ## defaultName" - "newName"
+  out_cols <- c(
+    "country_name" = "name",
+    "domain" = "topLevelDomain",
+    "iso2c" = "alpha2Code",
+    "iso3c" = "alpha3Code",
+    "calling_codes" = "callingCodes",
+    "alt_spelling" = "altSpellings",
+    "sub_region" = "subregion",
+    "native_name" = "nativeName",
+    "regional_blocs" = "regionalBlocs",
+    "numeric_code" = "numericCode"
+  )
+
+  if(length(out_list) == 0){
+
+    warning("No data was returned for any requested country. Returning empty data frame")
+    out_df <- as.data.frame(matrix(nrow = 0, ncol = length(out_cols)))
+    names(out_df) <- names(out_cols)
+
+  } else {
+
+    out_df <- out_list %>%
+      rename(
+        all_of(out_cols)
+      ) %>%
+      hoist(latlng,
+            lat = list(1),
+            lon = list(2)
+      )
+  }
+
+  out_df
+
+}
 
 
 
